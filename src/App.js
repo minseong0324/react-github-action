467f138fc6936db486cc53be1ba70158f4b271c3
minseong0324 <kwan0324@naver.com>
2023-05-20 00:08:46 +0900

    reactButtonApp

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+  // "disabled"라는 상태를 만들고, 초기값을 false로 설정합니다.
+  // 이 상태는 "-"와 "+" 버튼이 비활성화되어 있는지(즉, 클릭할 수 없는 상태인지) 나타냅니다.
+  const [disabled, setDisabled] = useState(false);
+
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <h3 data-testid='counter'>{count}</h3>
+        <button
+          data-testid='minus-button'
+          onClick={() => setCount((prev) => prev - 1)}
+          // disabled 속성을 설정하여 이 버튼이 비활성화 될 수 있도록 합니다.
+          // "disabled" 상태가 true이면 버튼은 비활성화되고, false이면 활성화됩니다.
+          disabled={disabled}
+        >
+          -
+        </button>
+        <button
+          data-testid='plus-button'
+          onClick={() => setCount((prev) => prev + 1)}
+          // "+" 버튼에도 disabled 속성을 설정하여 이 버튼이 비활성화 될 수 있도록 합니다.
+          disabled={disabled}
+        >
+          +
+        </button>
+        <div>
+          {/* 버튼 요소를 생성합니다. */}
+          <button
+            // 이 버튼의 배경색을 파란색으로 설정합니다.
+            style={{ backgroundColor: "blue" }}
+            // 데이터-testid 속성을 'on/off-button'으로 설정합니다.
+            // 이 속성은 테스트할 때 이 버튼을 식별하는 데 사용됩니다.
+            data-testid='on/off-button' // 이 버튼의 텍스트를 'on/off'로 설정합니다. on/off
+            // 이 버튼을 클릭하면 "setDisabled" 함수를 호출하여 "disabled" 상태를 현재와 반대로 변경합니다.
+            // 즉, 이 버튼을 클릭하면 "-"와 "+" 버튼의 활성화/비활성화 상태가 전환됩니다.
+            onClick={() => setDisabled((prev) => !prev)}
+          >
+            on/off
+          </button>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/App.test.js
@@ -0,0 +1,58 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import App from "./App";
+
+test("the counters starts at 0", () => {
+  render(<App />);
+  const counterElement = screen.getByTestId("counter");
+  expect(counterElement).toHaveTextContent(0); //toBe가 아니라..
+});
+
+test("minus button has correct text", () => {
+  render(<App />);
+
+  const buttonElement = screen.getByTestId("minus-button");
+  expect(buttonElement).toHaveTextContent("-");
+});
+
+test("plus button has correct text", () => {
+  render(<App />);
+  const buttonElement = screen.getByTestId("plus-button");
+  expect(buttonElement).toHaveTextContent("+");
+});
+
+test("When the + button is pressed, the counter changes to 1", () => {
+  render(<App />); // App 컴포넌트를 렌더링합니다.
+  const buttonElement = screen.getByTestId("plus-button");
+  // "plus-button"이라는 테스트 ID를 가진 요소를 화면에서 찾아서 buttonElement 변수에 할당합니다.
+  fireEvent.click(buttonElement);
+  // buttonElement에 클릭 이벤트를 발생시킵니다. 이는 실제 사용자가 "+" 버튼을 클릭하는 것을 시뮬레이션합니다.
+  const counterElement = screen.getByTestId("counter");
+  // "counter"라는 테스트 ID를 가진 요소를 화면에서 찾아서 counterElement 변수에 할당합니다.
+  expect(counterElement).toHaveTextContent(1);
+  // counterElement의 텍스트 내용이 1인지 확인합니다. "+" 버튼을 클릭했으므로, 카운터 값이 0에서 1로 증가했어야 합니다.
+});
+
+// 테스트를 정의합니다. "on/off 버튼이 파란색인지"를 테스트하는 것입니다.
+test("on/off button has blue color", () => {
+  // 우리의 App 컴포넌트를 렌더링합니다. 이것은 테스트를 시작하기 위해 필요합니다.
+  render(<App />);
+  // "on/off-button" 이라는 데이터-testid 속성을 가진 요소를 선택합니다. 이 요소는 on/off 버튼을 가리킵니다.
+  const buttonElement = screen.getByTestId("on/off-button");
+  // 이 버튼의 배경색이 파란색인지 확인합니다.
+  // 이 행은 "버튼 요소가 파란색 배경을 가지고 있는가?"를 테스트하는 expect 문입니다.
+  expect(buttonElement).toHaveStyle({ backgroundColor: "blue" });
+});
+
+// 테스트 케이스를 정의합니다. 이 테스트는 on/off 버튼이 클릭되었을 때 -,+ 버튼이 눌리지 않도록 막는지를 확인합니다.
+test("Prevent the -,+ button from being pressed when the on/off button is clicked", () => {
+  // 우선, App 컴포넌트를 렌더링합니다.
+  render(<App />);
+  // on/off 버튼 요소를 화면에서 찾아 변수에 저장합니다. 이 요소는 'on/off-button'이라는 testid를 가지고 있습니다.
+  const onOffButtonElement = screen.getByTestId("on/off-button");
+  // on/off 버튼 요소에 클릭 이벤트를 발생시킵니다.
+  fireEvent.click(onOffButtonElement);
+  // + 버튼 요소를 화면에서 찾아 변수에 저장합니다. 이 요소는 'plus-button'이라는 testid를 가지고 있습니다.
+  const plusButtonElement = screen.getByTestId("plus-button");
+  // 이제 + 버튼이 비활성화되었는지 확인합니다. on/off 버튼이 클릭되었으므로, + 버튼은 비활성화되어야 합니다.
+  expect(plusButtonElement).toBeDisabled();
+});
